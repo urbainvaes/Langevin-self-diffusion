065af50fc06f43428ecc6b704da8b1c552c5995e
Urbain Vaes <urbain@vaes.uk>
2020-11-21 14:49:30 +0100

    Add plotting code

---
+++ src/plot.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+γs = np.loadtxt("data/data_γs.txt")
+δs = np.loadtxt("data/data_δs.txt")
+
+D11_wi = np.loadtxt("data/data_D11_wi.txt")
+σ11_wi = np.loadtxt("data/data_σ11_wi.txt")
+D11_wo = np.loadtxt("data/data_D11_wo.txt")
+σ11_wo = np.loadtxt("data/data_σ11_wo.txt")
+
+fig, ax = plt.subplots()
+for iδ, δ in enumerate(δs):
+    fig, ax = plt.subplots()
+    ax.loglog(γs, σ11_wo[:, iδ], ".-")
+    ax.set_prop_cycle(None)
+    ax.loglog(γs, σ11_wi[:, iδ], ".--")
+    ax.set_xlabel("$γ$")
+    plt.show()

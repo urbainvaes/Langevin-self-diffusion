bbfc0f86f099a410b378539fc398176efe4e26cb
Urbain Vaes <urbain@vaes.uk>
2020-11-23 10:41:57 +0100

    Update deploy script

--- src/deploy.jl
+++ src/deploy.jl
@@ -4,8 +4,10 @@ end
 
 γs = round.(logspace(-3, 0, 10), sigdigits=3)
 
-
-δ = .04
+γs = [0.001, 0.00215, 0.00464, 0.01, 0.0215, 0.0464, 0.1, 0.215, 0.464, 1.0]
+δ = [-.04, -.08, -.16, -.32, -.64]
 for γ in γs
-    run(`tmux new-window -n "γ=$γ,δ=$δ " julia mc.jl $γ $δ`)
+    for δ in δs
+        run(`tmux new-window -n "γ=$γ,δ=$δ " julia mc2d.jl $γ $δ`)
+    end
 end

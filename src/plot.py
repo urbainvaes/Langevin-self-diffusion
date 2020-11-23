e3feb91e2be45ce2f98ca9d1c6cc8c3a42cd5ec6
Urbain Vaes <urbain@vaes.uk>
2020-11-23 10:08:28 +0100

    Improve plotting code

--- src/plot.py
+++ src/plot.py
@@ -10,12 +10,29 @@ D11_wo = np.loadtxt("data/data_D11_wo.txt")
 σ11_wo = np.loadtxt("data/data_σ11_wo.txt")
 
 fig, ax = plt.subplots()
+ax.set_prop_cycle(None)
 for iδ, δ in enumerate(δs):
-    ax.loglog(γs, D11_wo[:, iδ], ".-")
-    ax.set_prop_cycle(None)
+    coeffs = np.polyfit(np.log10(γs[:7]), np.log10(D11_wo[:7, iδ]), deg=1)
+    ax.loglog(γs, D11_wo[:, iδ], ".-", 
+            label="$\delta = {}, D \propto \gamma^{{ {:.2f} }}$".format(δ, coeffs[0]))
+ax.set_prop_cycle(None)
+for iδ, δ in enumerate(δs):
     ax.loglog(γs, D11_wi[:, iδ], ".--")
-    ax.set_xlabel("$γ$")
+ax.set_xlabel("$γ$")
+plt.legend()
+plt.savefig("diffusion.pdf")
 plt.show()
+
+for iδ, δ in enumerate(δs):
+    fig, ax = plt.subplots()
+    ax.set_prop_cycle(None)
+    coeffs = np.polyfit(np.log10(γs[:7]), np.log10(D11_wo[:7, iδ]), deg=1)
+    ax.loglog(γs, D11_wo[:, iδ], ".-")
+    ax.loglog(γs, D11_wi[:, iδ], ".--")
+    ax.loglog(γs, 10**coeffs[1] * γs**coeffs[0], "-", label="$\gamma^{{ {} }}$".format(coeffs[0]))
+    ax.set_xlabel("$γ$")
+    plt.legend()
+    plt.show()
 
 fig, ax = plt.subplots()
 for iδ, δ in enumerate(δs):

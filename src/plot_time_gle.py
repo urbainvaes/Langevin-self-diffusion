14e233065712172026949a50a6c9310b80b2e28e
Urbain Vaes <urbain@vaes.uk>
2021-08-12 17:37:33 +0200

    Minor change

--- src/plot_time_gle.py
+++ src/plot_time_gle.py
@@ -7,6 +7,7 @@ matplotlib.rc('font', family='serif')
 matplotlib.rc('text', usetex=True)
 # matplotlib.rc('figure', figsize=(15, 8))
 matplotlib.rc('figure', figsize=(12, 6))
+matplotlib.rcParams['text.latex.preamble']=[r"\usepackage{amsmath}"]
 plt.ion()
 
 ν = 2
@@ -36,7 +37,8 @@ def get_diff(γ, plot=True):
         nsigma = 3
         fig, ax = plt.subplots()
         ax.set_xlabel('$t$')
-        ax.set_title(r"Mean $\pm$ 3 standard deviations of $u(t)$ and $v(t)$ for $\gamma = 10^{" + str(int(np.log10(γ))) +  r"}$")
+        ax.set_title(r"MC estimation of $\mathbf{E}u(t)$ and $\mathbf{E}v(t)$ for $\gamma = 10^{" + str(int(np.log10(γ))) +  r"},$"
+                     + r" with $\pm 3 \sigma$ confidence interval")
         ax.plot(underdamped_ts, underdamped_D, ".-", c='red', label="MC/No control")
         ax.plot(underdamped_ts, underdamped_D_control, ".-", c='green', label="MC/Underdamped")
         ax.plot(underdamped_ts, 0*underdamped_ts + limit, "-", c='blue', lw=3, label="Conjectured limit")
@@ -48,11 +50,11 @@ def get_diff(γ, plot=True):
                         underdamped_D_control - nsigma*underdamped_σ_control/np.sqrt(nptotal),
                         underdamped_D_control + nsigma*underdamped_σ_control/np.sqrt(nptotal),
                         color='green', alpha=.2)
-        # ax.set_xlim([0, 100/γ])
-        # ax.set_ylim([2000, 4000])
+        ax.set_xlim([underdamped_ts[0], underdamped_ts[-1]])
+        ax.set_ylim([.2/γ, .4/γ])
         ax.legend()
         ax.grid()
-        fig.savefig("time-gle.pdf", bbox_inches='tight')
+        fig.savefig("time-gle" + str(int(np.log10(γ))) + ".pdf", bbox_inches='tight')
         plt.show()
 
     return underdamped_D_control[-1], underdamped_σ_control[-1]/np.sqrt(nptotal)
@@ -66,8 +68,9 @@ for i, γ in enumerate(γs):
 fig, ax = plt.subplots()
 ax.set_xlabel('$\gamma$')
 ax.semilogx(γs, γs*Ds, '.-')
+ax.set_xlim([γs[0], γs[-1]])
 ax.fill_between(γs, γs*Ds - 3*γs*σs, γs*Ds + 3*γs*σs, color='green', alpha=.2)
 
-get_diff(1e-3, plot=False)
-get_diff(1e-4, plot=False)
-get_diff(1e-5, plot=False)
+get_diff(1e-3, plot=True)
+get_diff(1e-4, plot=True)
+get_diff(1e-5, plot=True)

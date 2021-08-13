f63aa2be1e64262ed65ca070d1d6131b2949e931
Urbain Vaes <urbain@vaes.uk>
2021-08-13 11:31:30 +0200

    Remove unused script

--- src/plot_time_gle.py
+++ src/plot_time_gle.py
@@ -21,7 +21,6 @@ sample_size = {
         1e-2: 5000,
         1e-3: 5000,
         1e-4: 5000,
-        1e-5: 5000,
         }
 
 def get_diff(γ, plot=True):
@@ -36,7 +35,12 @@ def get_diff(γ, plot=True):
     underdamped_σ_control = data[:, 4]
 
     # Number of particles
-    nptotal = sample_size[γ]
+    if len(data[0]) > 5:
+        nptotal = data[:, 5]
+    else:
+        nptotal = np.zeros(len(underdamped_ts)) + sample_size[γ]
+
+    # Limit
     limit = lim_gle/γ
 
     if  plot:
@@ -49,12 +53,12 @@ def get_diff(γ, plot=True):
         ax.plot(underdamped_ts, underdamped_D_control, "-", c='green', label="MC/Underdamped")
         ax.plot(underdamped_ts, 0*underdamped_ts + limit, "--", c='blue', lw=3, label="Conjectured limit")
         ax.fill_between(underdamped_ts,
-                        underdamped_D - nsigma*underdamped_σ/np.sqrt(nptotal),
-                        underdamped_D + nsigma*underdamped_σ/np.sqrt(nptotal),
+                        underdamped_D - nsigma*underdamped_σ/np.sqrt(nptotal[i]),
+                        underdamped_D + nsigma*underdamped_σ/np.sqrt(nptotal[i]),
                         color='red', alpha=.2)
         ax.fill_between(underdamped_ts,
-                        underdamped_D_control - nsigma*underdamped_σ_control/np.sqrt(nptotal),
-                        underdamped_D_control + nsigma*underdamped_σ_control/np.sqrt(nptotal),
+                        underdamped_D_control - nsigma*underdamped_σ_control/np.sqrt(nptotal[i]),
+                        underdamped_D_control + nsigma*underdamped_σ_control/np.sqrt(nptotal[i]),
                         color='green', alpha=.2)
         ax.set_xlim([underdamped_ts[0], underdamped_ts[-1]])
         ax.set_ylim([.2/γ, .4/γ])
@@ -63,7 +67,7 @@ def get_diff(γ, plot=True):
         fig.savefig("time-gle" + str(int(np.log10(γ))) + ".pdf", bbox_inches='tight')
         plt.show()
 
-    return underdamped_D_control[-1], underdamped_σ_control[-1]/np.sqrt(nptotal)
+    return underdamped_D_control[-1], underdamped_σ_control[-1]/np.sqrt(nptotal[-1])
 
 γs = np.array([1e-5, 1e-4, 1e-3, 1e-2,1e-1, 1e-0])
 Ds = np.zeros(len(γs))
